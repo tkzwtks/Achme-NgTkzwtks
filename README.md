e06ff21590abca1ab46216b6a95eeb8fb9d6bbb4
tkzwtks <tkzwtks@gmail.com>
2019-12-06 22:16:39 +0900

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::NgTkzwtks - It's new $module
+
+# SYNOPSIS
+
+    use Acme::NgTkzwtks;
+
+# DESCRIPTION
+
+Acme::NgTkzwtks is ...
+
+# LICENSE
+
+Copyright (C) tkzwtks.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tkzwtks <tkzwtks@hatena.ne.jp>
---
+++ lib/Acme/NgTkzwtks.pm
@@ -0,0 +1,39 @@
+package Acme::NgTkzwtks;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::NgTkzwtks - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::NgTkzwtks;
+
+=head1 DESCRIPTION
+
+Acme::NgTkzwtks is ...
+
+=head1 LICENSE
+
+Copyright (C) tkzwtks.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tkzwtks E<lt>tkzwtks@hatena.ne.jpE<gt>
+
+=cut
+
